4a1b63f5c067e5ff64adc5cdb3239de5e35967da
Benjamin Morrise <ben.morrise@overops.com>
2020-06-05 13:46:57 -0400

    Build update

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,14 @@
-### set docker env
-eval $(minikube docker-env)
-
-### build the repository
-#mvn clean install -DskipTests
-
 ### mongodb
 kubectl apply -f mongo-deployment.yaml
 
 ### item-service
+sed -i "s/@IMAGE_VERSION@/$1/g" item-service/item-service-deployment.yaml
 kubectl apply -f item-service/item-service-deployment.yaml
 
 ### client-service
+sed -i "s/@IMAGE_VERSION@/$1/g" client-service/client-service-deployment.yaml
 kubectl apply -f client-service/client-service-deployment.yaml
 
 ### frontend
+sed -i "s/@IMAGE_VERSION@/$1/g" frontend/frontend-deployment.yaml
 kubectl apply -f frontend/frontend-deployment.yaml
-
-# Check that the pods are running
-kubectl get pods
-
-minikube service frontend-service
