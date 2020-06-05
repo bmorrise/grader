5b358f8519d09ceeeb350f34f3b7a8c329112394
Benjamin Morrise <ben.morrise@overops.com>
2020-06-05 14:16:21 -0400

    Updated

--- frontend/src/components/item/ItemComponent.tsx
+++ frontend/src/components/item/ItemComponent.tsx
@@ -13,16 +13,18 @@ class ItemComponent extends React.Component<IProps> {
     const completed = new Date(item.completed);
 
     return <div className="panel">
-      <div className="header">
-        <div className="title">{item.name}</div>
-        <div className="grade">{item.grade ? item.grade : "N/A"}</div>
+      <div className="panel-body">
+        <div className="header">
+          <div className="title">{item.name}</div>
+        </div>
+        <div className="body">
+          {item.notes}
+        </div>
+        <div className="footer">
+          Grading Completed: {(completed.getMonth() + 1) + "/" + completed.getDate() + "/" + completed.getFullYear()}
+        </div>
       </div>
-      <div className="body">
-        {item.notes}
-      </div>
-      <div className="footer">
-        Grading Completed: {(completed.getMonth() + 1) + "/" + completed.getDate() + "/" + completed.getFullYear()}
-      </div>
+      <div className="grade">{item.grade ? item.grade : "N/A"}</div>
     </div>;
   }
 
