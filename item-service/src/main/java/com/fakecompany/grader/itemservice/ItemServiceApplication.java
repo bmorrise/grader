a5bb717bec3983b37d1b83448ffd8298270567bc
Benjamin Morrise <ben.morrise@overops.com>
2020-06-10 11:16:38 -0400

    Changed collector location

--- item-service/src/main/java/com/fakecompany/grader/itemservice/ItemServiceApplication.java
+++ item-service/src/main/java/com/fakecompany/grader/itemservice/ItemServiceApplication.java
@@ -3,10 +3,27 @@ package com.fakecompany.grader.itemservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 @SpringBootApplication
 public class ItemServiceApplication {
 
 	public static void main(String[] args) {
+		ExecutorService executor = Executors.newCachedThreadPool();
+		executor.submit(() -> {
+			while (true) {
+				String threadName = Thread.currentThread().getName();
+				System.out.println(threadName);
+				Thread.sleep(5000);
+				try
+				{
+					throw new Exception("An error has occurred");
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 		SpringApplication.run(ItemServiceApplication.class, args);
 	}
 
